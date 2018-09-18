52e2380077e3ee52fa0fb660d1a9e16677cc37d6
Stanislas Polu <polu.stanislas@gmail.com>
2018-09-18 11:49:12 +0200

    rename state to symbolic

---
+++ symbolic/__init__.py
@@ -0,0 +1,14 @@
+from symbolic.constants import HIGHWAY_VOXEL_WIDTH
+from symbolic.constants import HIGHWAY_LANES_COUNT
+from symbolic.constants import HIGHWAY_DEPTH
+from symbolic.constants import HIGHWAY_WIDTH
+from symbolic.constants import HIGHWAY_HEIGHT
+
+from symbolic.object import ObjectType
+from symbolic.object import Object
+
+from symbolic.map import RoadType
+from symbolic.map import LineType
+from symbolic.map import Map
+
+from symbolic.highway import Highway
